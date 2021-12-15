613ddb53baac4418be35a80075761fde58bf0cf5
visionbike <phuc.visionbike@gmail.com>
2021-12-15 17:44:59 +0800

    update feature extraction part

--- feature_extraction.py
+++ feature_extraction.py
@@ -19,6 +19,7 @@ class MaxPeak:
             peak = np.expand_dims(peak, axis=1)
             if i == 0:
                 peaks = peak
+                continue
             peaks = np.hstack((peaks, peak))
         return peaks
 
@@ -40,6 +41,7 @@ class Mean:
             mean = np.expand_dims(mean, axis=1)
             if i == 0:
                 means = mean
+                continue
             means = np.hstack((means, mean))
         return means
 
@@ -61,6 +63,7 @@ class Variance:
             var = np.expand_dims(var, axis=1)
             if i == 0:
                 vars = var
+                continue
             vars = np.hstack((vars, var))
         return vars
 
@@ -82,6 +85,7 @@ class StandardDeviation:
             std = np.expand_dims(std, axis=1)
             if i == 0:
                 stds = std
+                continue
             stds = np.hstack((stds, std))
         return stds
 
@@ -103,6 +107,7 @@ class Skewness:
             skew = np.expand_dims(skew, axis=1)
             if i == 0:
                 skews = skew
+                continue
             skews = np.hstack((skews, skew))
         return skews
 
@@ -124,6 +129,7 @@ class Kurtosis:
             kurt = np.expand_dims(kurt, axis=1)
             if i == 0:
                 kurts = kurt
+                continue
             kurts = np.hstack((kurts, kurt))
         return kurts
 
@@ -145,6 +151,7 @@ class RootMeanSquare:
             rms = np.expand_dims(rms, axis=1)
             if i == 0:
                 rmss = rms
+                continue
             rmss = np.hstack((rmss, rms))
         return rmss
 
@@ -166,6 +173,7 @@ class WaveformLength:
             wl = np.expand_dims(wl, axis=1)
             if i == 0:
                 wls = wl
+                continue
             wls = np.hstack((wls, wl))
         return wls
 
@@ -196,6 +204,7 @@ class WillisonAmplitude:
             wamp = np.expand_dims(wamp, axis=1)
             if i == 0:
                 wamps = wamp
+                continue
             wamps = np.hstack((wamps, wamp))
         return wamps
 
@@ -230,6 +239,7 @@ class PSD:
                                        axis=1)
             if i == 0:
                 psds = power
+                continue
             psds = np.hstack((psds, power))
         return psds
 
@@ -261,7 +271,7 @@ class STFT:
                                         axis=1)
             if i == 0:
                 stfts = stft
-            print(stft.shape)
+                continue
             stfts = np.dstack((stfts, stft))
         return stfts
 
@@ -272,18 +282,30 @@ class CWT:
     """
 
     @classmethod
-    def apply(cls, segment, window_size=50, width=50):
+    def apply(cls, segment, window_size=50, widths=50):
         """
                 @param segment: signal segment.
                 @param window_size: window size. Default: 50.
-                @param width: frequency width. Default: 50.
+                @param widths: number of scale. Default: 50.
                 """
-        assert window_size <= 0 or window_size > segment.shape[1], 'window_size={} is invalid!'.format(window_size)
+        assert 0 <= window_size <= segment.shape[1], 'window_size={} is invalid!'.format(window_size)
         length = segment.shape[1]
+        num_channels = segment.shape[0]
+        if isinstance(widths, int):
+            widths = np.arange(1, widths + 1)
         cwts = np.array([])
         for i in range(0, length, window_size):
             start = i
             end = i + window_size if (i + window_size <= length) else 2 * i + window_size - length
-            cwt = signal.cwt(segment[:, start: end], signal.ricker, width, axis=0)
-            cwts = np.append(cwts, cwt)
+            cwt = np.array([])
+            for j in range(num_channels):
+                cwt_ = signal.cwt(segment[j, start: end], signal.ricker, widths, dtype=np.float64)
+                if j == 0:
+                    cwt = cwt_
+                    continue
+                cwt = np.dstack((cwt, cwt_))
+            cwt = np.transpose(cwt, (2, 0, 1))
+            if i == 0:
+                cwts = cwt
+            cwts = np.dstack((cwts, cwt))
         return cwts
